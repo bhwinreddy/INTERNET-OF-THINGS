7fa3a40d683d23668302b075e1cb4310c2c2c900
bhwinreddy <60635990+bhwinreddy@users.noreply.github.com>
2020-07-11 15:08:21 +0530

    Add files via upload

---
+++ serialmonitor1.ino
@@ -0,0 +1,50 @@
+/*
+  This program blinks pin 13 of the Arduino (the
+  built-in LED)
+*/
+
+void setup()
+
+{
+
+pinMode(13, OUTPUT);
+
+Serial.begin(9600);
+
+while (!Serial);
+
+Serial.println("Input 1 to Turn LED on and 2 to off");
+
+}
+
+void loop() {
+
+if (Serial.available())
+
+{
+
+int state = Serial.parseInt();
+
+if (state == 1)
+
+{
+
+digitalWrite(13, HIGH);
+
+Serial.println("Command completed LED turned ON");
+
+}
+
+if (state == 2)
+
+{
+
+digitalWrite(13, LOW);
+
+Serial.println("Command completed LED turned OFF");
+
+}
+
+}
+
+}
